dcaed3be13c0c023da984eafdddf1436087e9cf0
Aayush <falcon.bridge2000@gmail.com>
2022-02-16 22:37:54 +0530

    created a search form to get the location from user and get forecast info using client side JS

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,19 @@
-console.log("Client side JS is loaded");
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
 
-fetch("http://localhost:3000/weather?address=mumbai").then((response) => {
-  response.json().then((data) => {
-    if (data.error) {
-      console.log(data.error);
-    } else {
-      console.log(data);
+weatherForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const location = search.value;
+  fetch(`http://localhost:3000/weather?address=${location}`).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          console.log(data.error);
+        } else {
+          console.log(data);
+        }
+      });
     }
-  });
+  );
 });
