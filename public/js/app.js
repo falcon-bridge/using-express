4166fa1400749a450587c2c20441411957b10c7a
Aayush <falcon.bridge2000@gmail.com>
2022-02-16 22:16:17 +0530

    made request for forecast data in browser with client side JS(fetch API)

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,11 @@
 console.log("Client side JS is loaded");
+
+fetch("http://localhost:3000/weather?address=mumbai").then((response) => {
+  response.json().then((data) => {
+    if (data.error) {
+      console.log(data.error);
+    } else {
+      console.log(data);
+    }
+  });
+});
